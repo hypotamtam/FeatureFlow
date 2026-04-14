43e978e0eebc205236bfcd94c269aff5dcc7bd02
thomascassany <cassany.thomas@gmail.com>
2026-04-14 14:02:00 +1200

    chore: fix the documentation action

--- Package.swift
+++ Package.swift
@@ -21,6 +21,9 @@ let package = Package(
             targets: ["FeatureFlowTesting"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+    ],
     targets: [
         .target(
             name: "FeatureFlow"
